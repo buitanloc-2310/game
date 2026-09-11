1b0d138d80062418771e92182292e76fba419346
buitanloc-2310 <buitanloccon@gmail.com>
2026-09-11 20:39:53 +0700

    Delete game-main directory

--- game-main/dist/lib/router.js
+++
@@ -1,8 +0,0 @@
-export function getRoute() {
-    return { path: window.location.pathname || '/', query: new URLSearchParams(window.location.search) };
-}
-export function navigate(path) {
-    window.history.pushState({}, '', path);
-    window.dispatchEvent(new PopStateEvent('popstate'));
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
